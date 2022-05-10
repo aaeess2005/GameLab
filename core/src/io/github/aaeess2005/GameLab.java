fae907aba14817fddc1bf436544a6d4b4386a35d
aaeess2005 Chen <2145446114@qq.com>
2022-05-10 23:49:19 +0800

    A Simple Demo

--- core/src/io/github/aaeess2005/GameLab.java
+++ core/src/io/github/aaeess2005/GameLab.java
@@ -1,25 +1,41 @@
 package io.github.aaeess2005;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.CpuSpriteBatch;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameLab extends ApplicationAdapter {
+	final int width=960,height=540;
 	SpriteBatch batch;
 	Texture img;
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera=new OrthographicCamera();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		ScreenUtils.clear(0, 1, 1, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,0,0,this.width,this.height,0,1,1,0);
 		batch.end();
 	}
 	
@@ -27,5 +43,17 @@ public class GameLab extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+		camera.setToOrtho(false,this.width,this.height);
+		Gdx.graphics.setWindowedMode(height*16/9,height);
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
 	}
 }
--- desktop/src/io/github/aaeess2005/DesktopLauncher.java
+++ desktop/src/io/github/aaeess2005/DesktopLauncher.java
@@ -1,14 +1,18 @@
 package io.github.aaeess2005;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.github.aaeess2005.GameLab;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
+		//config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+		config.setWindowedMode(960,540);
+		config.setWindowPosition(120,120);
+		config.setResizable(true);
+		config.setForegroundFPS(0);
 		config.setTitle("GameLab");
 		new Lwjgl3Application(new GameLab(), config);
 	}
