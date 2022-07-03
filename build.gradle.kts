b2ec90a5f73fb99fe4eeb3cc45365229cb44378d
aaeess2005 Chen <2145446114@qq.com>
2022-07-03 17:11:03 +0800

    update

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("java")
+}
+
+group = "io.github.aaeess2005"
+version = "0.0.1"
+
+repositories {
+    //mavenLocal { setUrl("file://${project.rootDir}/lib") }
+    // 本地仓库
+    mavenLocal()
+    // 阿里云
+    maven("https://maven.aliyun.com/repository/public")
+    // Maven中央仓库
+    mavenCentral()
+}
+
+val lwjglVersion = "3.3.1"
+val jomlVersion = "1.10.4"
+
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } -> "natives-linux"
+        arrayOf("Windows").any { name.startsWith(it) } -> "natives-windows"
+        else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
+
+dependencies {
+    // Netty
+    implementation("io.netty:netty-all:4.1.78.Final")
+
+    // GSON
+    implementation("com.google.code.gson:gson:2.8.5")
+
+    // LWJGL
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-bgfx")
+    implementation("org.lwjgl", "lwjgl-egl")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-nanovg")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opencl")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-opengles")
+    implementation("org.lwjgl", "lwjgl-par")
+    implementation("org.lwjgl", "lwjgl-shaderc")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.lwjgl", "lwjgl-xxhash")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-bgfx", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nanovg", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengles", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-par", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-shaderc", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-xxhash", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+}
+
---
+++ src/main/java/io/github/aaeess2005/gamelab/Main.java
@@ -0,0 +1,7 @@
+package io.github.aaeess2005.gamelab;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
